768e21d3dd25b8b84a6ded9027fc3e1e0e94f775
sidarhalman <sidarhalman93@gmail.com>
2024-05-29 19:21:48 +0200

    dotenv package added and set

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import dotenv from "dotenv"
+
+
+
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT ;
+console.log(process.env)
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
